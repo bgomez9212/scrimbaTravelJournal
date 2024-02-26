2ab17b8df6dc1e018fa52503cb0e0aee2a826973
Brandon Gomez <105175292+bgomez9212@users.noreply.github.com>
2024-02-26 12:49:19 -0800

    Add files via upload

---
+++ Components/Card.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+export default function Card(props){
+    const {id, title, location, googleMapsUrl, startDate, endDate, description, imageUrl} = props
+    return (
+        <div className={`card-container ${id}`}>
+            <img src={imageUrl} />
+            <div className="text-container">
+                <img src="./icons/Path.png"/>
+                <span className="location">{location}</span>
+                <span><a className="google-maps" 
+                    href={{googleMapsUrl}}>View on Google Maps</a></span>
+                <h1 className="title">{title}</h1>
+                <p className="date">{startDate}</p>
+                <p className="desc">{description}</p>
+            </div>
+        </div>
+    )
+}
---
+++ Components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Nav(){
+    return (
+        <nav>
+            <img src="./icons/world-icon.png" />
+            <span>my hockey travel journal</span>
+        </nav>
+    )
+}
