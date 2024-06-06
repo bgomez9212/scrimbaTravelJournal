7d7c9b3f077b2fe73c94a810fbc382e37e9ad861
Brandon Gomez <bgomez9212@gmail.com>
2024-06-06 12:33:28 -0700

    fix photo links

--- data.js
+++ data.js
@@ -1,35 +1,41 @@
 export default [
-    {
-        key: 1,
-        id: "seattle",
-        title: "Climate Pledge Arena",
-        location: "Seattle",
-        googleMapsUrl: "https://goo.gl/maps/czUGNWEdU4QBxn7i6",
-        startDate: "December 11, 2021",
-        endDate: "",
-        description: "The Seattle Kraken vs. The Colombus Blue Jackets. The first professional hockey game I attended. Brand new stadium and team. The roof is the roof from the Key Arena. Aiming for a zero emissions arena. Cool things around the arena like the plant wall, lots of food options and the metal cups for beverages.",
-        imageUrl: "https://cms.nhl.bamgrid.com/images/photos/317262908/1284x722/cut.jpg"
-    },
-    {
-        key: 2,
-        id: "SJ",
-        title: "SAP Center",
-        location: "San Jose",
-        googleMapsUrl: "https://goo.gl/maps/YoErPCrfAaJ7EDQBA",
-        startDate: "February 27, 2022",
-        endDate: "",
-        description: "The Seattle Kraken vs. The San Jose Sharks. The legendary Shark Tank, otherwise known as the SAP Center. Loved this stadium, felt like what a hockey game should feel like. Place had a personality and the fans brought a culture. Specialty foods and cocktails, and commemorative cups. Great soundtrack too. Favorite arena I've been to.",
-        imageUrl: "https://cms.nhl.bamgrid.com/images/photos/299607688/2048x1152/cut.jpg"
-    },
-    {
-        key: 3,
-        id: "anaheim",
-        title: "Honda Center",
-        location: "Anaheim",
-        googleMapsUrl: "https://goo.gl/maps/GYp9ZV2PSFsgJkVo9",
-        startDate: "January 6, 2023",
-        endDate: "",
-        description: "The Anaheim Ducks vs The San Jose Sharks. Pretty underwhelming arena to me. Did not have a personality or a culture with the fanbase. Distracting lights while the puck was in play. Nothing to make the place standout or memorable, just your run-of-the-mill arena.",
-        imageUrl: "https://www.imageofsport.com/img-get/I0000h77EFU8ZNe8/s/1200/I0000h77EFU8ZNe8.jpg"
-    }
-]
+  {
+    key: 1,
+    id: "seattle",
+    title: "Climate Pledge Arena",
+    location: "Seattle",
+    googleMapsUrl: "https://goo.gl/maps/czUGNWEdU4QBxn7i6",
+    startDate: "December 11, 2021",
+    endDate: "",
+    description:
+      "The Seattle Kraken vs. The Colombus Blue Jackets. The first professional hockey game I attended. Brand new stadium and team. The roof is the roof from the Key Arena. Aiming for a zero emissions arena. Cool things around the arena like the plant wall, lots of food options and the metal cups for beverages.",
+    imageUrl:
+      "https://living-future.org/wp-content/uploads/2023/10/Climate-Pledge-ArenaWEST-AERIAL_final-scaled.jpg",
+  },
+  {
+    key: 2,
+    id: "SJ",
+    title: "SAP Center",
+    location: "San Jose",
+    googleMapsUrl: "https://goo.gl/maps/YoErPCrfAaJ7EDQBA",
+    startDate: "February 27, 2022",
+    endDate: "",
+    description:
+      "The Seattle Kraken vs. The San Jose Sharks. The legendary Shark Tank, otherwise known as the SAP Center. Loved this stadium, felt like what a hockey game should feel like. Place had a personality and the fans brought a culture. Specialty foods and cocktails, and commemorative cups. Great soundtrack too. Favorite arena I've been to.",
+    imageUrl:
+      "https://arenanetwork.net/wp-content/uploads/2011/07/SAP-1024x681.jpg",
+  },
+  {
+    key: 3,
+    id: "anaheim",
+    title: "Honda Center",
+    location: "Anaheim",
+    googleMapsUrl: "https://goo.gl/maps/GYp9ZV2PSFsgJkVo9",
+    startDate: "January 6, 2023",
+    endDate: "",
+    description:
+      "The Anaheim Ducks vs The San Jose Sharks. Pretty underwhelming arena to me. Did not have a personality or a culture with the fanbase. Distracting lights while the puck was in play. Nothing to make the place standout or memorable, just your run-of-the-mill arena.",
+    imageUrl:
+      "https://www.imageofsport.com/img-get/I0000h77EFU8ZNe8/s/1200/I0000h77EFU8ZNe8.jpg",
+  },
+];
